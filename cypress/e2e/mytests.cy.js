e6bb56d8ff2b9f97ad7d87bc441a8a9ee67ee7ea
Yaroslavcher <yaroslavcher@gmail.com>
2023-05-27 13:48:52 +0300

    fixed tests and added script

--- cypress/e2e/mytests.cy.js
+++ cypress/e2e/mytests.cy.js
@@ -4,37 +4,34 @@ describe("add tests", () => {
     cy.login("test@test.com", "test");
   });
     
-  it("Favorite page opened", () => {
+  it("1.Favorite page opened", () => {
     cy.get("span.pt-2").should("have.text", "Добро пожаловать test@test.com");
   });
 
-  it("1. add book and check as favorite", () => {
+  it("2.Add book and check as favorite", () => {
     cy.contains("Add new").click();
-    
     cy.contains("Book description").should("be.visible");
     cy.get("#title").type("title1").should("have.value", "title1");
     cy.get("#description").type("good").should("have.value", "good");
     cy.get("#authors").type("author1").should("have.value", "author1");
-    // cy.get('[type="checkbox"]')
-    //   .not("[disabled]")
-    //   .check()
-    //   .should("be.checked");
+    cy.get('[type="checkbox"]').check()
     cy.contains("Submit").click();
+    cy.get(".card-title.h5").should('include.text', 'title1')
+    cy.get("p.card-text").should('include.text', 'author1')
+    cy.contains("title1").should('be.visible')
+    cy.contains("author1").should('be.visible')
+    cy.contains('Delete from favorite').click()
+    cy.contains('Add to favorite').should('be.visible')
+    cy.contains('Favorites').click()
+    cy.contains('Please add some book to favorit on home page!').click()
+    cy.contains('Add new').should('be.visible').click()
+    cy.get('.modal-header .modal-title').should('have.text', 'Book description')
   });
 
-  it("2. open added book", () => {
+  it("3.Open added book", () => {
     cy.contains("Log out").click();
     cy.contains("Books list").should("be.visible");
     cy.contains("title1").should("be.visible");
     cy.contains("author1").should("be.visible");
   });
-
-  it("3. open Books list", () => {
-    cy.get("div.card-body").click();
-    cy.contains("title1").should("be.visible");
-    cy.contains("good").should("be.visible");
-    cy.contains("author1").should("be.visible");
-    cy.contains("Dowload book").should('be.visible').click();
-    cy.contains('Not Found').should('be.visible') 
-  });   
 });
